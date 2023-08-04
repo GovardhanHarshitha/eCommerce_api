76387e37fa49702d681b9f087caa218175085b5a
Venkat A Suresh <54540744+VenkataSuresh7989@users.noreply.github.com>
2023-08-04 20:18:46 +0530

    Update db details

--- db/DBConnection.py
+++ db/DBConnection.py
@@ -3,11 +3,17 @@ import pymysql
 
 def connect_db():
     try:
-        conn = pymysql.connect(host="localhost",
-                               user="root",
-                               password="root",
+        # conn = pymysql.connect(host="localhost",
+        #                        user="root",
+        #                        password="root",
+        #                        charset='utf8mb4',
+        #                        db="ecommerce",
+        #                        cursorclass=pymysql.cursors.DictCursor)
+        conn = pymysql.connect(host="sql9.freesqldatabase.com",
+                               user="sql9637478",
+                               password="EmLlGwxJGx",
                                charset='utf8mb4',
-                               db="ecommerce",
+                               db="sql9637478",
                                cursorclass=pymysql.cursors.DictCursor)
         print(conn)
         return conn
